6cbcd686169078d53a88ecff7e8621a7ae7c175b
Balaram Usov <dest@disroot.org>
2018-07-09 03:08:32 +0300

    Added some customizing features

--- server.py
+++ server.py
@@ -9,6 +9,8 @@ class WikiController:
         self.indexfile = None
         self.tchanged = None
         self.outhtml = None
+        self.title = ""
+        self.style = None
 
     def setDir(self, wikidir):
         self.mddir = wikidir
@@ -20,6 +22,12 @@ class WikiController:
     def setOut(self, outfile):
         self.outhtml = outfile
 
+    def setTitle(self, wikititle):
+        self.title = wikititle
+
+    def setStyle(self, stylename):
+        self.style = stylename
+
     def checkIndex(self):
         return (self.tchanged == os.stat(self.indexfile).st_mtime)
 
@@ -30,8 +38,10 @@ class WikiController:
             lp = os.path.join(self.mddir, l)
             gluemd += open(lp.split()[0]).read()
             gluemd += "\n\n"
-        m = Markdown(output_format="html")
+        m = Markdown(output_format="html")        
         htmlstr = m.convert(gluemd)
+        wrp = self._wrapping()
+        htmlstr = wrp[0] + htmlstr + wrp[1]
         outstream = open(self.outhtml, "w")
         outstream.write(htmlstr)
         outstream.close()
@@ -40,6 +50,11 @@ class WikiController:
     def update(self):
         if not self.checkIndex():
             self.forceUpdate()
+
+    def _wrapping(self):
+        return ('<!DOCTYPE html>\n<html>\n<meta charset="UTF-8">\n\n<head>\n<title>' + self.title + "</title>" +\
+               ('<link rel="stylesheet" type="text/css" href="' + self.style + '">' if self.style else "") +\
+                "</head>\n<body>", "</body>\n</html>")
 
 if __name__ == "__main__":
     import time
@@ -50,11 +65,15 @@ if __name__ == "__main__":
     WIKI_DIR = os.path.join(os.getcwd(), "wiki")
     INDEX_FILE = os.path.join(WIKI_DIR, "index.txt")
     OUT_HTML = os.path.join(WIKI_DIR, "index.html")
+    CSS_STYLE = os.path.join(WIKI_DIR, "style.css")
+    WIKI_TITLE = "Wiki"
 
     wc = WikiController()
     wc.setDir(WIKI_DIR)
     wc.setIndex(INDEX_FILE)
     wc.setOut(OUT_HTML)
+    wc.setStyle(CSS_STYLE)
+    wc.setTitle(WIKI_TITLE)
     wc.forceUpdate()
 
     while True:
